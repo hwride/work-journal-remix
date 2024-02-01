47f410fc8e0903f9e8c0b262cbb314d6f8435f41
Hugo Wride <hugowride@gmail.com>
2024-02-01 08:25:05 +0000

    Add basic edit page with loader

--- app/routes/entries.$entryId.edit.tsx
+++ app/routes/entries.$entryId.edit.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return <p>Editing</p>;
+import { PrismaClient } from "@prisma/client";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const db = new PrismaClient();
+  const entry = await db.entry.findUnique({
+    where: { id: Number(params.entryId) },
+  });
+
+  if (entry == null) {
+    throw new Response("Not found", { status: 404 });
+  }
+
+  return {
+    ...entry,
+    date: entry?.date.toISOString().substring(0, 10),
+  };
+}
+
+export default function EditPage() {
+  const entry = useLoaderData<typeof loader>();
+
+  return (
+    <div className="mt-4">
+      <p>Editing entry {entry.id}</p>
+    </div>
+  );
 }
